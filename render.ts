6a30e06aa17058f17d2a6d00ca9c7b46d6fa9f84
sandrasm4 <sorias@LAPTOP-2UDPQKV1>
2023-04-23 11:50:51 +0200

    finish

--- render.ts
+++ render.ts
@@ -1,4 +1,4 @@
-import { Receta, RecetaDetails } from "./recetas.js";
+import { Digest, Ingredient, Receta, RecetaDetails } from "./recetas.js";
 const columnNum = 3;
 
 type tplotOptions = {
@@ -28,7 +28,7 @@ const renderRecipes = (recetas: Array<Receta>) => {
   let cont = 0;
   let html = "";
   html += `
-  <div class=titulo>Recetas del Mundo</div>
+  <div class=titulo>World Recipes</div>
   <table>
     <tr>`;
   for (const receta of recetas) {
@@ -50,46 +50,49 @@ const renderRecipes = (recetas: Array<Receta>) => {
 const renderSpecifications = (detail: RecetaDetails) => {
   let html = "";
   html += `
-  <div id="Specifications" class="tabcontent">
+  <div id="specifications" class="tabcontent">
+    <p class="titulo">Cuisine Type: ${detail.cuisineType}</p>
     <p class="titulo">Diet Labels:</p>
-    <table><tr>`
+    <div id="diet">`
     for(let label of detail.dietLabels){
-      html+= `<td><img src="${dietLabel[label]}"/></td>`
+      html+= `<img id="diet" src="${dietLabel[label]}"/>`
     }
-    html+=`</tr></table>
-    <p class="titulo">Health Labels:</p>`
+    html+=`</div>
+    <p class="titulo">Health Labels:</p><p>`
     for(let label of detail.healthLabels){
-      html+= `<p>${label}</p>`
+      html+= `${label}, `
     }
-    html+=`
-    <p>cautions:</p>
-    <p>${detail.cautions}</p>
-    <table><tr><td class="titulo">Calories</td><td class="titulo">Meal Type</td><td class="titulo">Dish Type</td></tr>
+    if(detail.cautions.length!=0){
+      html+=`</p>
+      <p class="titulo">
+      <img src="https://uxwing.com/wp-content/themes/uxwing/download/signs-and-symbols/warning-icon.png"/></td>
+      Cautions:
+      </p>
+      <p>${detail.cautions}</p>`
+    }else{
+      html+=`</p>`
+    }
+    html+=`<table><tr><td class="titulo">Calories</td><td class="titulo">Meal Type</td><td class="titulo">Dish Type</td></tr>
     <tr><td><img src="https://cdn-icons-png.flaticon.com/512/2316/2316951.png"/></td>
     <td><img src="https://cdn-icons-png.flaticon.com/512/3073/3073843.png"/></td>
-    <td><img src="https://image.similarpng.com/very-thumbnail/2020/12/Vegetable-salad-icon-illustration.png"/></td></tr>
+    <td><img src="https://img.freepik.com/vector-premium/ensalada-cayendo-tazon_202271-2216.jpg?w=2000"/></td></tr>
     <tr><td>${detail.calories}</td><td>${detail.mealType}</td><td>${detail.dishType}</td></tr>
     </table>
-    <p>Cuisine Type:${detail.cuisineType}</p>`
-    for(let d of detail.digest){
-      html+= `<p>${d.label}:${d.total}${d.units}</p>`
-    }  
-  html+=`</div>`
+  </div>`
   return html;
 }
 
-const renderIngredients = (detail: RecetaDetails) => {
+const renderIngredients = (ingredients: Ingredient[]) => {
   let html = "";
   html += `
-  <div id="Ingredients" class="tabcontent">
+  <div id="ingredients" class="tabcontent">
   <table>
     <th>Image</th>
     <th>Food</th>
-    <th>Text</th>
     <th>Food Category</th>
     <th>Quantity</th>
     <th>Weight</th>`
-    for(let ingredient of detail.ingredients){
+    for(let ingredient of ingredients){
       html+= `<tr><td><img src="${ingredient.image}"/></td>
               <td>${ingredient.food}</td>
               <td>${ingredient.foodCategory}</td>
@@ -102,37 +105,60 @@ const renderIngredients = (detail: RecetaDetails) => {
 const renderHow = (detail: RecetaDetails) => {
   let html = "";
   html += `
-  <div id="How" class="tabcontent">
-    <p>Yield:${detail.yieldV}</p>
-
-    <p>Total Time: ${detail.totalTime}</p>
-
-    <p>Instructions:</p>`
+  <div id="how" class="tabcontent">
+    <table>
+      <td><img src="https://static.thenounproject.com/png/4502200-200.png"/></td>
+      <td><p>Yield: ${detail.yieldV} </p>
+      <p>Total Time: ${detail.totalTime} </p></td>
+    </table>
+    <p>Instructions:</p><ol>`
     if(detail.instructions){
       for(let inst of detail.instructions){
-        html += `<p>${inst}</p>`
+        html += `<li>${inst}</li>`
       }
+    }else{
+      html += `<li>The API doesn't have this information yet.</li>
+      <li>Put all the ingredients together and mix them.
+      <img src="https://cdn-icons-png.flaticon.com/512/5622/5622660.png"/></li>`
     }
-    html+=`</div>`
+    html+=`</ol></div>`
     return html;
   }
 
+  const renderNutrients = (digest: Digest[]) => {
+    let html = "";
+    html += `
+    <div id="nutrients" class="tabcontent">
+      <table>`
+      for(let d of digest){
+        html+= `<tr>
+          <td>${d.label}</td>
+          <td>${d.total}`
+        if(d.units){
+          html+=d.units
+        }
+        html+= `  </td>
+        </tr>`
+      } 
+      html+=`</table></div>`
+      return html;
+  }
 
-const renderScript = () => {
-  return `<script>
-    function openTab(evt, tabName) {
-      var i, tabcontent, tablinks;
-      tabcontent = document.getElementsByClassName("tabcontent");
-      for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+  const renderScript = () => {
+    return `<script>
+      function openTab(evt, tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
       }
-      tablinks = document.getElementsByClassName("tablinks");
-      for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-      }
-      document.getElementById(tabName).style.display = "block";
-      evt.currentTarget.className += " active";
-    }
   </script>`;
 }
 
@@ -154,13 +180,15 @@ export const renderRecipe = (receta: Receta) => {
   ${head("Recetas List", "receta")}
   <body>
   <div class="tab">
-    <button class="tablinks" onclick="openTab(event, 'Specifications')">Specifications</button>
-    <button class="tablinks" onclick="openTab(event, 'Ingredients')">Ingredients</button>
-    <button class="tablinks" onclick="openTab(event, 'How')">How to cook it!</button>
+    <button class="tablinks" onclick="openTab(event, 'specifications')">Specifications</button>
+    <button class="tablinks" onclick="openTab(event, 'ingredients')">Ingredients</button>
+    <button class="tablinks" onclick="openTab(event, 'how')">How to cook it!</button>
+    <button class="tablinks" onclick="openTab(event, 'nutrients')">Nurtitional values</button>
   </div>
     ${renderSpecifications(receta.details)}
-    ${renderIngredients(receta.details)}
+    ${renderIngredients(receta.details.ingredients)}
     ${renderHow(receta.details)}
+    ${renderNutrients(receta.details.digest)}
     ${renderScript()}
   </body>
 </html>`;
